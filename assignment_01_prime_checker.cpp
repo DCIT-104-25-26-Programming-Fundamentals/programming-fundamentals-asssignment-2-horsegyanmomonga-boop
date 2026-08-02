5cb56d2747d670d741b40de95e6bde0512c12868
horsegyanmomonga-boop <horsegyanmomonga@gmail.com>
2026-08-02 20:59:45 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,36 @@
 #include <iostream>
 using namespace std;
 
+// Function declaration/definition to check if a number is prime
+bool isPrime(int n) {
+    // Numbers less than 2 are not prime
+    if (n < 2) {
+        return false;
+    }
+    
+    // Check divisors from 2 up to sqrt(n)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false; // Found a factor, so it's not prime
+        }
+    }
+    
+    return true; // No factors found, it's prime
+}
+
+int main() {
+    int number;
+    
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+    
+    return 0;
+}
+
+
